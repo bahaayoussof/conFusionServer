37a361c7a84500a8ae64a7be9a3a3f23d9a2c888
Bahaa Youssof <bahaayoussof@gmail.com>
2021-07-14 21:11:39 +0200

    Express REST API with MongoDB and Mongoose Part 1

---
+++ models/dishes.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+var commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        require: true
+    },
+    comment: {
+        type: String,
+        require: true
+    },
+    author: {
+        type: String,
+        require: true
+    }
+}, {
+    timestamp: true
+});
+
+const dishSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        requierd: true
+    },
+    image: {
+        type: String,
+        requierd: true
+    },
+    category: {
+        type: String,
+        requierd: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    comments: [commentSchema]
+}, {
+    timestamp: true
+});
+
+var Dishes = mongoose.model('Dish', dishSchema);
+
+module.exports = Dishes;
